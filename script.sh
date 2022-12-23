c11c2506a0985403695a94433d0001b49838a3c6
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-23 15:19:59 +0530

    Debugging

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 export CLOUDFIX_FILE=true
 export CLOUDFIX_TERRAFORM_LOCAL=true 
 raw_recco=$(./cloudfix-linter/cloudfix-linter recco | tail +2)
-res=$(gh api repos/trilogy-group/terraform-github-actions/issues/${pr_number}/comments \
+res=$(gh api repos/${{ repository }}/issues/${pr_number}/comments \
             -f body="${raw_recco}")
 echo "${res}"
