3f0e8c9ae16d51d2212eec608e178843acf05c84
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-23 15:31:19 +0530

    Fixing bugs

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ export CLOUDFIX_FILE=true
 export CLOUDFIX_TERRAFORM_LOCAL=true 
 raw_recco=$(./cloudfix-linter/cloudfix-linter recco | tail +2)
 echo "gh api repos/${repository}/issues/${pr_number}/comments -f body="${raw_recco}""
-# res=$(gh api repos/${repository}/issues/${pr_number}/comments \
-#             -f body="${raw_recco}")
-# echo "${res}"
+res=$(gh api repos/${repository}/issues/${pr_number}/comments \
+            -f body="${raw_recco}")
+echo "${res}"
