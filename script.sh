44d2c577cbb8a6deb174e955b71ebcd9d406d764
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-26 13:20:36 +0530

    Adding markup to comment

--- script.sh
+++ script.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+terraform_init=$(terraform init)
+echo "Terraform Init\n${terraform_init}"
+
+terraform_validate=$(terraform validate -no-color)
+echo "Terraform Validate\n${terraform_validate}"
+
+terraform_show=$(terraform show -json | head -2 | tail -1 > tf.show)
+echo "Terraform Validate\n${terraform_show}"
+
+mock_recco=$(python gen_recco.py tf.show)
+echo "Generate Mock Reccomendations\n${mock_recco}"
+
+install=$(bash install.sh)
+echo "Installing tflint and cloudfix-linter\n${install}"
+
+linter_init=$(./cloudfix-linter/cloudfix-linter init)
+echo "Cloudfix-Linter init\n${linter_init}"
+
 export CLOUDFIX_FILE=true
 export CLOUDFIX_TERRAFORM_LOCAL=true 
 raw_recco=$(./cloudfix-linter/cloudfix-linter recco | tail +2)
-echo "gh api repos/${repository}/issues/${pr_number}/comments -f body="${raw_recco}""
+markup_recco=$(python beautifier.py "${raw_recco}")
+echo "gh api repos/${repository}/issues/${pr_number}/comments -f body="${markup_recco}""
 res=$(gh api repos/${repository}/issues/${pr_number}/comments \
-            -f body="${raw_recco}")
+            -f body="${markup_recco}")
 echo "${res}"
