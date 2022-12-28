d5dc53d0534d6cf22f873299b66115c8476f88ca
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-28 17:43:02 +0530

    Test

--- script.sh
+++ script.sh
@@ -1,53 +1,57 @@
 #!/bin/bash
-pwd
 terraform_init=$(terraform init)
 status=$?
-echo "Terraform Init\n${terraform_init}"
 if [ $status != 0 ] 
 then
+    echo "Terraform Init Error\n${terraform_init}"
     exit 1
 fi
+echo "Terraform Init\n"
 
 terraform_validate=$(terraform validate -no-color)
 status=$?
-echo "Terraform Validate\n${terraform_validate}"
 if [ $status != 0 ] 
 then
+    echo "Terraform Validate Error\n${terraform_validate}"
     exit 1
 fi
+echo "Terraform Validate\n${terraform_validate}"
 
 terraform_show=$(terraform show -json | head -2 | tail -1 > tf.show)
 status=$?
-echo "Terraform Show\n"
 if [ $status != 0 ] 
 then
+    echo "Terraform Show Error\n${terraform_show}"
     exit 1
 fi
+echo "Terraform Show\n"
 
 mock_recco=$(python gen_recco.py tf.show)
 status=$?
-echo "Generate Mock Reccomendations\n${mock_recco}"
 if [ $status != 0 ] 
 then
+    echo "Error in Mock recommendation\n${mock_recco}"
     exit 1
 fi
+echo "Generated Mock Recommendations\n"
 
 install=$(bash installScript.sh)
 status=$?
 if [ $status != 0 ] 
 then
+    echo "install error\n ${install}"
     exit 1
 fi
-echo "Installingg tflint and cloudfix-linter\n"
-
+echo "Installed tflint and cloudfixLinter"
 
 linter_init=$(./cloudfix-linter/cloudfix-linter init)
 status=$?
-echo "Cloudfix-Linter init\n${linter_init}"
 if [ $status != 0 ] 
 then
+    echo "Cloudfix-Linter init\n${linter_init}"
     exit 1
 fi
+echo "Cloudfix-Linter initialized"
 
 export CLOUDFIX_FILE=true
 export CLOUDFIX_TERRAFORM_LOCAL=true 
@@ -56,10 +60,8 @@ markup_recco=$(python beautifier.py "${raw_recco}")
 res=$(gh api repos/${repository}/issues/${pr_number}/comments \
             -f body="${markup_recco}")
 status=$?
-echo "${res}"
 if [ $status != 0 ] 
 then
+    echo "API ERROR ${res}"
     exit 1
 fi
-
-pwd
