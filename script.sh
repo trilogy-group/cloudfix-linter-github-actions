5a14f27ce4c0c4f188ab9835aaba64014be6f42f
prasheel-ti <prasheel.tiwari@trilogy.com>
2022-12-28 10:59:19 +0530

    Removing extra files

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export CLOUDFIX_FILE=true
-export CLOUDFIX_TERRAFORM_LOCAL=true 
-raw_recco=$(./cloudfix-linter/cloudfix-linter recco | tail +2)
-res=$(gh api repos/trilogy-group/terraform-github-actions/issues/${pr_number}/comments \
-            -f body="${raw_recco}")
-echo "${res}"
